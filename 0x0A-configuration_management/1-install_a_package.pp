cb043e24e035470b7844f4f6aac0b6cbf28b5551
islamhasib953 <islamhasib953@gmail.com>
2024-04-12 19:07:55 +0200

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Using Puppet, install flask from pip3.
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
